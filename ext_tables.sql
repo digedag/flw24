b16c38fcb96cd571d24ecadac6940225c8182921
René Nitzsche <rene@system25.de>
2015-09-02 13:56:23 +0200

    Autorenfelder für News-Berichte

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,9 @@
 CREATE TABLE tx_cfcleague_games (
 	newspreview2 int(11) DEFAULT '0' NOT NULL,
-	newsreport2 int(11) DEFAULT '0' NOT NULL
+	newsreport2 int(11) DEFAULT '0' NOT NULL,
+	newspreviewauthor varchar(100) DEFAULT '' NOT NULL,
+	newsreportauthor varchar(100) DEFAULT '' NOT NULL,
+	newspreviewauthor2 varchar(100) DEFAULT '' NOT NULL,
+	newsreportauthor2 varchar(100) DEFAULT '' NOT NULL
 );
 
