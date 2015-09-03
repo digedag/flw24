375bec16417e52b63ca5ac39783e9077f1d885b6
René Nitzsche <rene@system25.de>
2015-09-03 21:19:43 +0200

    Autorenfelder wieder entfernt

--- Configuration/TCA/Overrides/tx_cfcleague_games.php
+++ Configuration/TCA/Overrides/tx_cfcleague_games.php
@@ -28,50 +28,8 @@ $columns = array(
 					'maxitems' => 1,
 			)
 	),
-
-	'newspreviewauthor' => Array (
-			'exclude' => 1,
-			'label' => 'LLL:EXT:flw24/Resources/Private/Language/locallang_db.xml:tx_cfcleague_games_newspreviewauthor',
-			'config' => Array (
-					'type' => 'input',
-					'size' => '30',
-					'max' => '100',
-					'eval' => 'trim',
-			)
-	),
-	'newsreportauthor' => Array (
-			'exclude' => 1,
-			'label' => 'LLL:EXT:flw24/Resources/Private/Language/locallang_db.xml:tx_cfcleague_games_newsreportauthor',
-			'config' => Array (
-					'type' => 'input',
-					'size' => '30',
-					'max' => '100',
-					'eval' => 'trim',
-			)
-	),
-	'newspreviewauthor2' => Array (
-			'exclude' => 1,
-			'label' => 'LLL:EXT:flw24/Resources/Private/Language/locallang_db.xml:tx_cfcleague_games_newspreviewauthor2',
-			'config' => Array (
-					'type' => 'input',
-					'size' => '30',
-					'max' => '100',
-					'eval' => 'trim',
-			)
-	),
-	'newsreportauthor2' => Array (
-			'exclude' => 1,
-			'label' => 'LLL:EXT:flw24/Resources/Private/Language/locallang_db.xml:tx_cfcleague_games_newsreportauthor2',
-			'config' => Array (
-					'type' => 'input',
-					'size' => '30',
-					'max' => '100',
-					'eval' => 'trim',
-			)
-	),
-
 );
 
 tx_rnbase_util_Extensions::addTCAcolumns('tx_cfcleague_games',$columns,1);
-tx_rnbase_util_Extensions::addToAllTCAtypes('tx_cfcleague_games','newsreportauthor,newspreviewauthor, newspreview2, newspreviewauthor2, newsreport2, newsreportauthor2','','after:newsreport');
+tx_rnbase_util_Extensions::addToAllTCAtypes('tx_cfcleague_games','newspreview2, newsreport2','','after:newsreport');
 
