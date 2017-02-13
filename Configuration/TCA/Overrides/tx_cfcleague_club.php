8b1e5637c2818d889acc38b4a064bc2dd123a9bf
René Nitzsche <rene@system25.de>
2017-02-13 15:27:56 +0100

    Add feusers to club

---
+++ Configuration/TCA/Overrides/tx_cfcleague_club.php
@@ -0,0 +1,34 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+tx_rnbase::load('tx_rnbase_util_Extensions');
+
+$columns = array(
+	'feusers' => Array (
+		'exclude' => 1,
+		'label' => 'LLL:EXT:flw24/Resources/Private/Language/locallang_db.xml:tx_cfcleague_club_feusers',
+		'config' => Array (
+				'type' => 'group',
+				'internal_type' => 'db',
+				'allowed' => 'fe_users',
+				'size' => 5,
+				'autoSizeMax' => 20,
+				'minitems' => 0,
+				'maxitems' => 100,
+				'MM' => 'tx_cfcleague_club2feusers_mm',
+				'MM_match_fields' => Array (
+					'tablenames' => 'fe_users',
+				),
+				'wizards' => Tx_Rnbase_Utility_TcaTool::getWizards('tx_cfcleague_club', array(
+					'suggest' => true,
+				)
+			)
+		)
+	),
+);
+
+
+
+tx_rnbase_util_Extensions::addTCAcolumns('tx_cfcleague_club',$columns,1);
+tx_rnbase_util_Extensions::addToAllTCAtypes('tx_cfcleague_club','feusers','','after:favorite');
+
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,3 +3,20 @@ CREATE TABLE tx_cfcleague_games (
 	newsreport2 int(11) DEFAULT '0' NOT NULL
 );
 
+CREATE TABLE tx_cfcleague_club (
+	feusers int(11) DEFAULT '0' NOT NULL
+);
+
+#
+# Table structure for table 'tx_cfcleague_club2feusers_mm'
+# uid_local used for club
+#
+CREATE TABLE tx_cfcleague_club2feusers_mm (
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	tablenames varchar(50) DEFAULT '' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
+);
